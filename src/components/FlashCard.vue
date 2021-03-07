7cb933f8a88341795a6c6f9891e41b07230980fa
Devco2011 <devco2011@gmail.com>
2021-03-07 17:55:05 -0600

    set up

---
+++ src/components/FlashCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card" :class="{ flipped: card.answerShown }">
+    <div class="card-inner">
+      <div class="question">
+        <h4>Hello</h4>
+        <div>
+          <button @click="handleClick">Show Answer</button>
+        </div>
+      </div>
+      <div class="answer">
+        <h4>Answer</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card"],
+  methods: {
+    handleClick() {
+      this.$emit("toggle", this.card);
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  background-color: transparent;
+  width: 300px;
+  height: 200px;
+  border: 1px solid #f1f1f1;
+  perspective: 1000px;
+  margin-top: 20px;
+}
+
+.flipped .card-inner {
+  transform: rotateY(180deg);
+}
+</style>
---
+++ src/components/TriviaGame.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box">
+    <h1>Star Wars Trivia</h1>
+    <div v-for="card in trivia" :key="card.id">
+      <flash-card :trivia="card" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { trivia } from "../trivia";
+import FlashCard from "./FlashCard";
+
+export default {
+  components: { FlashCard },
+
+  data() {
+    return {
+      trivia: [...trivia],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
